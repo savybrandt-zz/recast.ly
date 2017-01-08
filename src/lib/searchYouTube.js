365a27241b35e23021bb9c7fd51653b3f6bafa79
savybrandt <savybrandt@gmail.com>
2017-01-07 16:23:18 -0800

    Completed: passed search youtube tests

--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -1,5 +1,18 @@
 var searchYouTube = (options, callback) => {
-  // TODO
+  console.log('options', options);
+  $.ajax({
+    url: 'https://www.googleapis.com/youtube/v3/search',
+    type: 'GET',
+    data: {part: 'snippet', type: 'video', key: options.key, q: options.query, maxResults: options.max},
+    contentType: 'application/json',
+    success: function (data) {
+      console.log('success: received: ', data);
+      callback(data.items);
+    },
+    error: function (data) {
+      console.error('failed to receive: ', data);
+    }
+  });
 };
 
 window.searchYouTube = searchYouTube;
